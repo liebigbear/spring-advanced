e8746afb7be98222dab1f11b63e24d02bd9ff93c
liebigbear <jtw10155@naver.com>
2025-07-22 19:58:11 +0900

    refactor(user): Validation
    비밀번호 유효성 검사를 해당 API request DTO에서 처리

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserChangePasswordRequest {
 
-    @NotBlank
+    @NotBlank(message = "기존 비밀번호는 필수 값입니다.")
     private String oldPassword;
-    @NotBlank
+    @NotBlank(message = "새 비밀번호는 필수 값입니다.")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\\\d).*$", message = "비밀번호는 대문자와 숫자를 최소 1개 이상 포함해야 합니다.")
     private String newPassword;
 }
